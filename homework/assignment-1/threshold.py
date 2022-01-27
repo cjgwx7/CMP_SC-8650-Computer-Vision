b58f0e3a3a1e25109f05298015d8992cac16a609
Caleb Grohmann <cgrohmann@mail.missouri.edu>
2022-01-27 00:21:58 -0600

    argument parser build v1

--- homework/assignment-1/threshold.py
+++ homework/assignment-1/threshold.py
@@ -1,5 +1,30 @@
-import os
-import numpy as np
 
-print("The current working directory is below:")
-print(os.getcwd())
+import argparse
+import cv2
+import numpy as np
+import sys
+import os
+
+### Initialize argument parser
+parser = argparse.ArgumentParser(description="Perform Grayscale Image Thresholding")
+
+### Create required argument group
+requiredNamed = parser.add_argument_group("Required named arguments")
+
+### Add arguments
+requiredNamed.add_argument("-i", "--image", help="Path to input image", required=True)
+requiredNamed.add_argument("-t", "--threshold", help="Pixel threshold value", type=int, required=True)
+parser.add_argument("-o", "--output", help="Optional path for output image")
+
+### Parse arguments
+args = parser.parse_args()
+
+### Save arguments
+image_path = args.image
+threshold = args.threshold
+output_path = args.output
+
+print(image_path, threshold, output_path)
+
+
+
